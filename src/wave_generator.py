9a1596aea9eeabf5e27aecaab08defe0ed7a9f50
Sándor Földi <sandorfoldi98@gmail.com>
2023-05-05 13:40:06 +0200

    backup

--- src/wave_generator.py
+++ src/wave_generator.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp, solve_bvp
 
+from tqdm import tqdm
+
 
 def gen_wave_data_ivp(
     c: float, x0: float, x1: float, t1: float, dt: float, dx: float, ic: Callable
@@ -46,7 +48,7 @@ def ic_sin(a, b):
     return lambda x: np.sin(a * x + b)
 
 
-if __name__ == "__main__":
+def generate_simulation():
     ic_func = ic_sin(1, 0)
     sensors = np.linspace(-np.pi, np.pi, 10)
     y, x, t = gen_wave_data_ivp(
@@ -69,3 +71,70 @@ if __name__ == "__main__":
     plt.imshow(y)
     plt.show()
     # wave_eq_discovery()
+
+
+def generate_simulation_1(i, a, b):
+    ic_func = ic_sin(a, b)
+    sensors = np.linspace(-np.pi, np.pi, 10)
+    y, x, t = gen_wave_data_ivp(
+        c=1, x0=-np.pi, x1=np.pi, t1=100, dt=1, dx=0.063, ic=ic_func
+    )
+
+    u = sense_func(ic_func, sensors)
+    x = np.stack([x, t], axis=1)
+    
+    np.save(f'data/{i}_x.npy', x)
+    np.save(f'data/{i}_y.npy', y)
+    np.save(f'data/{i}_u.npy', u)
+
+
+def generate_simulation2(i):
+    ic_func = ic_sin(1, 0)
+    sensors = np.linspace(-np.pi, np.pi, 10)
+    y, x, t = gen_wave_data_ivp(
+        c=1, x0=-np.pi, x1=np.pi, t1=100, dt=1, dx=0.063, ic=ic_func
+    )
+
+    u = sense_func(ic_func, sensors)
+    data = np.array(
+        [(x, t, y, u)],
+        dtype=[
+            ("x", np.ndarray),
+            ("t", np.ndarray),
+            ("y", np.ndarray),
+            ("u", np.ndarray),
+        ],
+    )
+    np.save(f"data/{i}.npy", data)
+
+def get_points():
+    num_points = 100
+    arr_size = 100000
+    train_as = np.random.choice(np.linspace(0.1, 10, arr_size), 100, replace=False)
+    train_bs = np.random.choice(np.linspace(-np.pi, np.pi, arr_size), 100, replace=False)
+
+    train_xs = np.random.choice(list(range(100)), 100000, replace=True)
+    train_ts = np.random.choice(list(range(100)), 100000, replace=True)
+
+    return train_as, train_bs, train_xs, train_ts
+
+
+
+
+def generate_dataset():
+    train_as = np.random.choice(np.linspace(0.1, 10, 100000), 100, replace=False)
+    train_bs = np.random.choice(np.linspace(-np.pi, np.pi, 100000), 100, replace=False)
+    i = list(range(100))
+
+    for a, b, i in tqdm(zip(train_as, train_bs, i)):
+        # generate_simulation_1(i, a, b)
+        generate_simulation2(i)
+
+
+        
+
+
+
+if __name__ == "__main__":
+    # generate_simulation()
+    generate_dataset()
--- src/wave_loader.py
+++ src/wave_loader.py
@@ -1,38 +1,93 @@
 import torch
 import numpy as np
+from glob import glob
 
 
-class WaveLoader(torch.utils.data.IterableDataset):
-    def __init__(self, paths, params,):
-        super(WaveLoader, self).__init__()
-        self.params = params
-        self.data = []
-        for path in paths:
+class WaveLoader(torch.utils.data.Dataset):
+    def __init__(self, paths, params, device='cpu', splits = (0.8, 0.2)):
+        # load and shuffle paths
+        paths = np.array(paths)
+        np.random.shuffle(paths)
+
+        # create and shuffle x (place) and t (time) indices
+        x_idxs = np.array(range(100))
+        t_idxs = np.array(range(100))
+
+        np.random.shuffle(x_idxs)
+        np.random.shuffle(t_idxs)
+
+        xt_idxs = np.array([(x, t) for x in x_idxs for t in t_idxs])
+
+        # split paths, x_idxs, and t_idxs into train and val
+        assert len(splits) == 2 and sum(splits) == 1, 'splits must be a tuple of length 2 and sum to 1'
+
+        train_paths = paths[:int(len(paths) * splits[0])]
+        val_paths = paths[int(len(paths) * splits[0]):]
+
+        train_xt_idxs = xt_idxs[:int(len(xt_idxs) * splits[0])].reshape([-1, 100, 2])
+        val_xt_idxs = xt_idxs[int(len(xt_idxs) * splits[0]):].reshape([-1, 100, 2])
+
+        # load train data
+        self.train_xts = []
+        self.train_ys = []
+        self.train_us = []
+
+        assert len(train_paths) == len(train_xt_idxs), 'train_paths and train_xt_idxs must be the same length'
+        for path, xt_idx in zip(train_paths, train_xt_idxs):
             data = np.load(path, allow_pickle=True)
-            self.data.append({'x': data['x'], 't': data['t'], 'y': data['y'], 'u': data['u'],})
+            x, t, y, u = data['x'][0], data['t'][0], data['y'][0], data['u'][0]
+
+            self.train_xts.append(np.stack([x[xt_idx[:, 0]], t[xt_idx[:, 1]]], axis=1))
+            self.train_ys.append(y[xt_idx[:, 0], xt_idx[:, 1]])
+            self.train_us.append([u]*len(xt_idx))
+
+        self.train_xts = np.array(self.train_xts).reshape([-1, 2])
+        self.train_ys = np.array(self.train_ys).reshape([-1, 1])
+        self.train_us = np.array(self.train_us).reshape([-1, 10])
+
+        
+        # load val data
+        self.val_xts = []
+        self.val_ys = []
+        self.val_us = []
+
+        assert len(val_paths) == len(val_xt_idxs), 'val_paths and val_xt_idxs must be the same length'
+        for path, xt_idx in zip(val_paths, val_xt_idxs):
+            data = np.load(path, allow_pickle=True)
+            x, t, y, u = data['x'][0], data['t'][0], data['y'][0], data['u'][0]
+
+            self.val_xts.append(np.stack([x[xt_idx[:, 0]], t[xt_idx[:, 1]]], axis=1))
+            self.val_ys.append(y[xt_idx[:, 0], xt_idx[:, 1]])
+            self.val_us.append([u]*len(xt_idx))
+        
+        self.val_xts = np.array(self.val_xts).reshape([-1, 2])
+        self.val_ys = np.array(self.val_ys).reshape([-1, 1])
+        self.val_us = np.array(self.val_us).reshape([-1, 10])
+
+        
+    def __len__(self):
+        return len(self.train_xts) + len(self.val_xts)
     
-    @staticmethod
-    def random_item(arr):
-        return arr[np.random.randint(0, len(arr))]
+    def __getitem__(self, idx):
+
+
+        
+
+
+
+
+
+
+
+
 
 
     def __next__(self):
-        # choose a random data point should be random
-        idx_sim = np.random.randint(0, len(self.data))
-        sim  = self.data[idx_sim]
-        
-        idx_x = np.random.randint(0, len(sim['x']))
-        idx_t = np.random.randint(0, len(sim['t']))
-
-        x = sim['x'][0][idx_x]
-        t = sim['t'][0][idx_t]
-        y = sim['y'][0][idx_x, idx_t]
-        u = sim['u'][0]
-
-        return x, t, y, u
+        pass
 
 if __name__ == '__main__':
-    loader = WaveLoader(['data/0.npy'], {})
+    paths = glob('data/*.npy')
+    loader = WaveLoader(paths, {})
     for i in range(10):
         x, t, y, u = next(loader)
         pass
