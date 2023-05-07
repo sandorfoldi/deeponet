2309a6149a5f5f3765652e2a9b71844ad20b6659
Sigurd Thorlund <46456752+SigurdThorlund@users.noreply.github.com>
2023-05-07 19:16:10 +0200

    Commit b4 integrating changes from main

--- src/model.py
+++ src/model.py
@@ -4,15 +4,24 @@ import torch
 class DeepONet(torch.nn.Module):
     def __init__(self, n_sensors, n_hidden, n_output):
         super(DeepONet, self).__init__()
+        # Parameters
+        self.n_sensors = n_sensors
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+
         # Branch net
         self.B1 = torch.nn.Linear(n_sensors, n_hidden)
         self.B2 = torch.nn.Linear(n_hidden, n_hidden)
-        self.B3 = torch.nn.Linear(n_hidden, n_output)
+        self.B3 = torch.nn.Linear(n_hidden, n_hidden)
+        self.B4 = torch.nn.Linear(n_hidden, n_hidden)
+        self.B5 = torch.nn.Linear(n_hidden, n_output)
 
         # Trunk net
         self.T1 = torch.nn.Linear(2, n_hidden)
         self.T2 = torch.nn.Linear(n_hidden, n_hidden)
         self.T3 = torch.nn.Linear(n_hidden, n_output)
+        self.T4 = torch.nn.Linear(n_hidden, n_output)
+        self.T5 = torch.nn.Linear(n_hidden, n_output)
 
         self.activation = torch.sin
 
@@ -25,18 +34,24 @@ class DeepONet(torch.nn.Module):
         # Pass through branch net
         b = self.activation(self.B1(u))
         b = self.activation(self.B2(b))
-        b = self.B3(b)
+        b = self.activation(self.B3(b))
+        b = self.activation(self.B4(b))
+        b = self.B5(b)
 
         # Pass through trunk net
         t = self.activation(self.T1(y))
         t = self.activation(self.T2(t))
-        t = self.T3(t)
+        t = self.activation(self.T3(t))
+        t = self.activation(self.T4(t))
+        t = self.T5(t)
 
         # Combine
         if self.batch:
             output = torch.einsum('ij,ij->i', b, t)
         else:
             output = b @ t
-            
 
         return output
+    
+    def __repr__(self):
+        return 'DON_{}_{}_{}'.format(self.n_sensors, self.n_hidden, self.n_output)
