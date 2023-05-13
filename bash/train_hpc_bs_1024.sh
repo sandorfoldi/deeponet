82ea6d085a3a22c8d800b291a46d96b41f5622e3
sandor <sandorfoldi98@gmail.com>
2023-05-13 19:05:05 +0200

    add bsub scripts

--- bash/train_hpc_bs_1024.sh
+++ bash/train_hpc_bs_1024.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#BSUB -J TrainHpcDefault
-#BSUB -W 6:00
+#BSUB -J BS_1024
+#BSUB -W 1:00
 #BSUB -q gpuv100
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -R "select[gpu32gb]"
@@ -8,9 +8,9 @@
 #BSUB -n 1
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
-echo
+echo "loading python"
 module load python3/3.9.11
-source ../env/bin/activate
+source env/bin/activate
 
 echo "Running script..."
-python3 ./src/train.py dataset=1000 model=deeponet_dense
+python src/model_training.py --dataset data/1000 --batch_size 1024
---
+++ bash/train_hpc_bs_64.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#BSUB -J BS_64
+#BSUB -W 1:00
+#BSUB -q gpuv100
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -R "select[gpu32gb]"
+#BSUB -gpu "num=1"
+#BSUB -n 1
+#BSUB -o logs/%J.out
+#BSUB -e logs/%J.err
+echo "loading python"
+module load python3/3.9.11
+source env/bin/activate
+
+echo "Running script..."
+python src/model_training.py --dataset data/1000 --batch_size 64
