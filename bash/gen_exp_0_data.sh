280e215107130cfddff920fe48bff45454bf79d3
Sándor Földi <sandorfoldi98@gmail.com>
2023-06-27 13:43:07 +0200

    add data gen script

---
+++ bash/gen_exp_0_data.sh
@@ -0,0 +1,4 @@
+python src/wave_generator_gm.py --root data/a --m_min 10 --m_max 90 --v_min 10 --v_max 10 --num_g 1
+python src/wave_generator_gm.py --root data/b --m_min 10 --m_max 90 --v_min 10 --v_max 10 --num_g 10
+python src/wave_generator_gm.py --root data/c --m_min 20 --m_max 80 --v_min 1 --v_max 20 --num_g 10
+python src/wave_generator_gm.py --root data/d --m_min 20 --m_max 80 --v_min 1 --v_max 20 --num_g 100s
