d3c1fc39365fccdc9f1e0e0d14c799a67d78e2c3
Sándor Földi <sandorfoldi98@gmail.com>
2023-06-27 15:21:33 +0200

    implement viz 16 simulations feature

--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import os
+from glob import glob
 
 
 def viz_animate(y: np.ndarray, x: np.ndarray, t: np.ndarray) -> None:
@@ -21,13 +23,11 @@ def viz_animate(y: np.ndarray, x: np.ndarray, t: np.ndarray) -> None:
         plt.pause(0.01)
     return None
 
-
 def viz_static(u: np.ndarray, x: np.ndarray, t: np.ndarray) -> None:
     """
     Visualize the wave data using animation
     """
-    # u = u.T
-    # assert u.shape == (len(t) - 1, len(x) - 1)
+    u = u.T  # Transpose u to match the dimensions
     xx, tt = np.meshgrid(x, t)
     fig, ax = plt.subplots()
     print(x.shape, t.shape, u.shape)
@@ -41,17 +41,48 @@ def viz_static(u: np.ndarray, x: np.ndarray, t: np.ndarray) -> None:
     plt.show()
     return None
 
+def viz_static_16(us, xs, ts) -> None:
+    """
+    Visualize the wave data using animation
+    """
+
+    fig, axs = plt.subplots(4, 4, figsize=(6, 6))
+    for u, x, t, ax in zip(us, xs, ts, axs.flatten()):
+        u = u.T  # Transpose u to match the dimensions
+        xx, tt = np.meshgrid(x, t)
+        ax.pcolormesh(xx, tt, u, shading="auto")
+        ax.set_xlabel("x")
+        ax.set_ylabel("t")
+        ax.set_title("u")
+
+    plt.tight_layout()
+    plt.show()
+    return None
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default="data/default/0.npy")
-    parser.add_argument("--mode", type=str, default="animate")
+    parser.add_argument("--data", type=str, default="data/a")
+    parser.add_argument("--mode", type=str, default="static_16")
     args = parser.parse_args()
-    data = np.load(args.data, allow_pickle=True)
-    x, t, y, u = data[0]
     if args.mode == "animate":
+        data = np.load(args.data, allow_pickle=True)
+        x, t, y, u = data[0]
         viz_animate(y, x, t)
     elif args.mode == "static":
+        data = np.load(args.data, allow_pickle=True)
+        x, t, y, u = data[0]
         viz_static(y, x, t)
+    elif args.mode == "static_16":
+        data_paths = glob(os.path.join(args.data, '*.npy'))
+        datas = [np.load(data_paths[i], allow_pickle=True) for i in range(16)]
+        xs, ts, ys, us = [], [], [], []
+        for i in range(16):
+            xs.append(datas[i][0][0])
+            ts.append(datas[i][0][1])
+            ys.append(datas[i][0][2])
+            us.append(datas[i][0][3])
+        viz_static_16(ys, xs, ts)
     else:
         raise ValueError("Invalid mode")
