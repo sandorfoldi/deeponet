32c34b58e7197965dac176e0a4605b6c59b0a059
Sándor Földi <sandorfoldi98@gmail.com>
2023-06-28 12:41:43 +0200

    package project

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+#
+from setuptools import setup, find_packages
+
+setup(
+    name="deeponet",
+    version="0.1.0",
+    description="Deeponet for wave equation",
+    author="Sandor Foldi, Sigurd Thorlund",
+    packages=find_packages(),
+    install_requires=[],
+)
