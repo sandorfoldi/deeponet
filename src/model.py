99a68c9fe38ebc2d04d22f4bad79f56acc417dff
sandor <sandorfoldi98@gmail.com>
2023-08-26 15:24:06 +0200

    pinn finite difference test implemented

--- src/model.py
+++ src/model.py
@@ -1,5 +1,5 @@
 import torch
-from wave_loader import get_wave_datasets
+from src.wave_loader import get_wave_datasets
 
 class DeepONet1DCNN(torch.nn.Module):
     """
@@ -185,6 +185,58 @@ class DeepONet(torch.nn.Module):
         output = torch.einsum('ij,ij->i', b, t)
 
         return output
-    
+
+
+class DeepONetNoBatchNorm(torch.nn.Module):
+    def __init__(self, n_sensors, n_hidden, n_output):
+        super(DeepONetNoBatchNorm, self).__init__()
+        # Parameters
+        self.n_sensors = n_sensors
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+
+        # Branch net
+        self.B1 = torch.nn.Linear(n_sensors, n_hidden)
+        self.B2 = torch.nn.Linear(n_hidden, n_hidden)
+        self.B3 = torch.nn.Linear(n_hidden, n_hidden)
+        self.B4 = torch.nn.Linear(n_hidden, n_hidden)
+        self.B5 = torch.nn.Linear(n_hidden, n_output)
+
+        # Trunk net
+        self.T1 = torch.nn.Linear(2, n_hidden)
+        self.T2 = torch.nn.Linear(n_hidden, n_hidden)
+        self.T3 = torch.nn.Linear(n_hidden, n_hidden)
+        self.T4 = torch.nn.Linear(n_hidden, n_hidden)
+        self.T5 = torch.nn.Linear(n_hidden, n_output)
+
+
+
+        self.activation = torch.sin
+
+    def set_batch(self, batch):
+        self.batch = batch
+
+    def forward(self, u, y):
+        # Pass through branch net
+        b = self.activation(self.B1(u))
+        b = self.activation(self.B2(b))
+        b = self.activation(self.B3(b))
+        b = self.activation(self.B4(b))
+        b = self.B5(b)
+
+        # Pass through trunk net
+        t = self.activation(self.T1(y))
+        t = self.activation(self.T2(t))
+        t = self.activation(self.T3(t))
+        t = self.activation(self.T4(t))
+        t = self.T5(t)
+
+        # Combine
+        output = torch.einsum('ij,ij->i', b, t)
+
+        return output
+
+
+
     def __repr__(self):
         return 'DON_Dense_{}_{}_{}'.format(self.n_sensors, self.n_hidden, self.n_output)
--- tests/test_pinn.py
+++ tests/test_pinn.py
@@ -1,9 +1,10 @@
 from glob import glob
+import numpy as np
 
 import torch
 from torch.nn import Module
 from src.pinn import make_loss_col_wave_eq
-from src.wave_loader import get_wave_datasets
+from src.model import DeepONetNoBatchNorm
 
 class Pinn0(Module):
     def __init__(self):
@@ -33,28 +34,33 @@ def test_pinn_1():
     
     assert d2dx2_preds == 2 * u[0][0]**2
     assert d2dt2_preds == 2 * u[0][1]**2
-    
+
 
 def todo_test_pinn_2():
-    # compare to finite difference on simple simulation
-    paths = glob('/work3/s216416/deeponet/data/test_1a/*')
+    path = '/work3/s216416/deeponet/data/1c/1.npy'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    ds_train, ds_valid = get_wave_datasets(paths, n_points=10, device=device)
+    data = np.load(path, allow_pickle=True)
+    xs, ts, ys, us = data['x'][0], data['t'][0], data['y'][0], data['u'][0]
 
-    # Train and validation loaders
-    train_dataloader =  torch.utils.data.DataLoader(ds_train, batch_size=32, shuffle=True)
-
-    xt_batch, y_batch, u_batch = next(iter(train_dataloader))
-
+    xs, ts, ys, us = torch.tensor([xs], device=device, dtype=torch.float32), torch.tensor([ts], device=device, dtype=torch.float32), torch.tensor([ys], device=device, dtype=torch.float32), torch.tensor([us], device=device, dtype=torch.float32)
     loss_col = make_loss_col_wave_eq(5)
-    net = Pinn0()
+    net = DeepONetNoBatchNorm(n_sensors=100, n_hidden=128, n_output=128)
 
-    d2dt2_preds, d2dx2_preds, loss = loss_col(net, u_batch, xt_batch, debug=True)
+    idx_x = 200
+    idx_t = 100
 
+    # finite difference second order derivatives
+    d2y_dt2 = (ys[0][idx_x, idx_t+1] - 2 * ys[0][idx_x, idx_t] + ys[0][idx_x, idx_t-1]) / (2 * (ts[0][idx_t] - ts[0][idx_t-1]))
+    d2y_dx2 = (ys[0][idx_x+1, idx_t] - 2 * ys[0][idx_x, idx_t] + ys[0][idx_x-1, idx_t]) / (2 * (xs[0][idx_x] - xs[0][idx_x-1]))
 
-def todo_test_pinn_3():
-    pass
+    print(f'd2y_dt2:\t\t\t{d2y_dt2:.4e}')
+    print(f'd2y_dx2:\t\t\t{d2y_dx2:.4e}')
+    print(f'd2y_dt2 - c^2 * d2y_dx2:\t{d2y_dt2 - 0.5**2 * d2y_dx2:.4e}')
+    print('---------------------------------------')
+    print('Estimating c based on finite difference')
+    print(f'c = sqrt(d2y_dt2 / d2y_dx2) =\t{np.sqrt(d2y_dt2 / d2y_dx2):.4e}')
+
 
 
 if __name__ == "__main__":
-    test_pinn_1()
+    todo_test_pinn_2()
